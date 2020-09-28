36c13ffd56aada8bc37c792837484d067a70792e
Reinier Carmona <reinier@generalsoftwareinc.com>
2020-09-28 13:52:53 -0400

    referencia al fichero ips subido con local-exec desde terraform

--- getdata.sh
+++ getdata.sh
@@ -1,5 +1,5 @@
 #/bin/bash
 NOW=$(TZ=America/Havana date +"%d%m%Y_%H%M")
 mkdir output
-for i in `cat ips`; do nslookup $i | grep name | awk '{print $4}' > output/nslookupip-$NOW-$i ; done 
-for i in `cat ips`; do mtr --report $i | grep -v ^Start > output/tracerip-$NOW-$i ; done
+for i in `cat ../ips`; do nslookup $i | grep name | awk '{print $4}' > output/nslookupip-$NOW-$i ; done 
+for i in `cat ../ips`; do mtr --report $i | grep -v ^Start > output/tracerip-$NOW-$i ; done
