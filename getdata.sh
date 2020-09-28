d70ffbe074a364719b83f32e54195de9fee26bfe
Reinier Carmona <reinier@generalsoftwareinc.com>
2020-09-28 14:44:42 -0400

    agregando un sleep para dar tiempo q local-exec copie el file ips

--- getdata.sh
+++ getdata.sh
@@ -1,5 +1,6 @@
 #/bin/bash
 NOW=$(TZ=America/Havana date +"%d%m%Y_%H%M")
+sleep 60
 mkdir output
 for i in `cat ../ips`; do nslookup $i | grep name | awk '{print $4}' > output/nslookupip-$NOW-$i ; done 
 for i in `cat ../ips`; do mtr --report $i | grep -v ^Start > output/tracerip-$NOW-$i ; done
