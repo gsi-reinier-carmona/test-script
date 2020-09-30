60ae78aa53d606c8a7565f37f22dcea751e019a8
Reinier Carmona <reinier@generalsoftwareinc.com>
2020-09-30 11:33:20 -0400

    modificando nslookup para obtener salida de un dominio

--- getdata.sh
+++ getdata.sh
@@ -4,6 +4,6 @@ echo '-w "dnslookup: %{time_namelookup} | connect: %{time_connect} | appconnect:
 NOW=$(TZ=America/Havana date +"%d%m%Y_%H%M")
 sleep 60
 mkdir output
-for i in `cat ../ips`; do nslookup $i | grep name | awk '{print $4}' > output/nslookupip-$NOW-$i ; done 
+for i in `cat ../ips`; do nslookup $i | grep Address | grep -v 127.0.0.53| awk '{print $2}' > output/nslookupip-$NOW-$i ; done 
 for i in `cat ../ips`; do mtr --report $i | grep -v ^Start > output/tracerip-$NOW-$i ; done
 for i in `cat ../ips`; do curl -so /dev/null $i > output/outputcurl-$NOW-$i; done
